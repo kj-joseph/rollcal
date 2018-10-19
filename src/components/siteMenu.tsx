dfd67b448fe856d2c3722d25534a5c45424a31f9
Kevin Joseph <kj.joseph@gmail.com>
2018-10-19 02:24:28 -0700

    Revamp menu items regarding login

--- src/components/siteMenu.tsx
+++ src/components/siteMenu.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import MenuContact from "images/menu/contact.svg";
-import MenuEdit from "images/menu/edit.svg";
-import MenuFaq from "images/menu/faq.svg";
-import MenuLogin from "images/menu/login.svg";
-import MenuLogout from "images/menu/logout.svg";
-import MenuProfile from "images/menu/profile.svg";
-import MenuSearch from "images/menu/search.svg";
+import LoginIconSolid from "images/menu/user-circle-solid.svg";
+import LoginIconOutline from "images/menu/user-circle.svg";
+import ReactSVG from "react-svg";
 
 import axios, { AxiosError, AxiosPromise, AxiosRequestConfig, AxiosResponse } from "axios";
 
@@ -16,135 +12,82 @@ export default class SiteMenu<Props> extends React.Component<any, any, any> {
 	constructor(props: Props) {
 		super(props);
 
-		this.logout = this.logout.bind(this);
-		this.openLoginBox = this.openLoginBox.bind(this);
+		this.state = {
+			errorMessage: "",
+			formValid: false,
+			loading: false,
+			loginEmail: "",
+			loginOpen: false,
+			loginPassword: "",
+			registerEmail: "",
+			registerPassword: "",
+			registerPasswordConfirm: "",
+			registerUsername: "",
+			status: "login",
+		};
 
-		if (sessionStorage.rollCalUserId
-			&& sessionStorage.rollCalUserName
-			&& sessionStorage.rollCalUserPermissions
-			&& sessionStorage.rollCalToken) {
-
-			axios.post(this.props.apiLocation + "auth/checkToken",
-				{
-					id: sessionStorage.rollCalUserId,
-					permissions: sessionStorage.rollCalUserPermissions,
-					username: sessionStorage.rollCalUserName,
-				},
-				{
-					headers: {
-						Authorization: `Bearer ${sessionStorage.rollCalToken}`,
-					},
-			}).then((results: AxiosResponse) => {
-				this.props.setUserInfo({
-					loggedIn: true,
-					loggedInUserId: sessionStorage.rollCalUserId,
-					loggedInUserName: sessionStorage.rollCalUserName,
-					loggedInUserPermissions: sessionStorage.rollCalUserPermissions.split(","),
-				});
-
-			}).catch((error: AxiosError) => {
-
-				if (sessionStorage.rollCalUserId) {
-					this.logout(null, sessionStorage.rollCalUserId);
-				} else {
-					this.logout();
-				}
-
-			});
-
-		}
+		this.openLoginModal = this.openLoginModal.bind(this);
 
 	}
 
-	logout(event?: React.MouseEvent<HTMLAnchorElement>, id?: number): void {
+	openLoginModal(event?: React.MouseEvent<HTMLAnchorElement>) {
 
 		if (event) {
 			event.preventDefault();
 		}
 
-		if (id) {
-			axios.delete(this.props.apiLocation + "auth/logout/" + id);
-		}
+		this.props.setLoginModalState(true);
 
-		sessionStorage.removeItem("rollCalUserId");
-		sessionStorage.removeItem("rollCalUserName");
-		sessionStorage.removeItem("rollCalUserPermissions");
-		sessionStorage.removeItem("rollCalToken");
-
-		this.props.setUserInfo({
-			loggedIn: false,
-			loggedInUserId: "",
-			loggedInUserPermissions: "",
-		});
 	}
 
-	openLoginBox(event: React.MouseEvent<HTMLAnchorElement>): void {
-		event.preventDefault();
-		this.props.setLoginBoxState(true);
-		this.props.setMenuState(false);
-	}
+	isEventsCurrentPage(path: any, match: any, location: any) {
 
-	isEventsLinkActive(path: any, match: any, location: any) {
-
-		return match && !match.pathname.match(/\/(?:search|faq|contact)/);
+		return match && !match.pathname.match(/\/(?:search|faq|contact|dashboard|validate)/);
 	}
 
 	render() {
 
 		return (
-			<div className="siteMenu">
-				<ul>
-					<li>
-						<NavLink to="/" title="Upcoming Events" activeClassName="active" isActive={this.isEventsLinkActive.bind(this)}>
-							Events
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/search" title="Search Events" activeClassName="active">
-							Search
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/faq" title="Frequently Asked Questions" activeClassName="active">
-							FAQ
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/contact" title="Contact Roll-Cal" activeClassName="active">
-							Contact
-						</NavLink>
-					</li>
-					{ 0 ? <React.Fragment>
-					{ this.props.loggedIn ?
-		<React.Fragment>
+
+			<React.Fragment>
+
+				<div className="siteMenu">
+					<ul>
 						<li>
-							<a href="" onClick={this.logout} title="Log out">
-								<img src={MenuLogout} alt="" />
-							</a>
-						</li>
-						<li>
-							<NavLink to="/profile" title="Profile">
-								<img src={MenuProfile} alt="" />
+							<NavLink to="/" title="Upcoming Events" activeClassName="active" isActive={this.isEventsCurrentPage.bind(this)}>
+								Events
 							</NavLink>
 						</li>
 						<li>
-							<NavLink to="/edit" title="Edit">
-								<img src={MenuEdit} alt="" />
+							<NavLink to="/search" title="Search Events" activeClassName="active">
+								Search
 							</NavLink>
 						</li>
-					</React.Fragment>
-					:
-					<React.Fragment>
 						<li>
-							<a href="" onClick={this.openLoginBox} title="Login / Register">
-								<img src={MenuLogin} alt="" />
-							</a>
+							<NavLink to="/faq" title="Frequently Asked Questions" activeClassName="active">
+								FAQ
+							</NavLink>
 						</li>
-					</React.Fragment>
-					}
-		</React.Fragment> : "" }
-				</ul>
-			</div>
+						<li>
+							<NavLink to="/contact" title="Contact Roll-Cal" activeClassName="active">
+								Contact
+							</NavLink>
+						</li>
+						<li className="desktopOnly">
+							{this.props.loggedIn ?
+								<NavLink to="/dashboard" title="Dashboard" activeClassName="activeIcon">
+									<ReactSVG src={LoginIconSolid} />
+								</NavLink>
+							:
+								<a href="" onClick={this.openLoginModal} title="Login / Register">
+									<ReactSVG src={LoginIconOutline} />
+								</a>
+							}
+						</li>
+					</ul>
+				</div>
+
+			</React.Fragment>
 		);
 
 	}
