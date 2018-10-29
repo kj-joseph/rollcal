42c54b618dfe371d994f0db16ac9ec4d78648eae
Kevin Joseph <kjoseph@rainbird.com>
2018-10-29 14:57:15 -0700

    Add email for account re-validation to local mailer output

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -22,11 +22,27 @@ export const sendValidationEmail = (email: string, username: string, validationC
 		`,
 		subject: "Validate your Roll-Cal account",
 		to: `${username} <${email}>`,
-	});
+	}, null);
 
 };
 
-export const sendEmailChangeEmail = () => {};
+export const sendEmailChangeEmail = (email: string, username: string, validationCode: string) => {
 
+	const validationUrl = `http://www.roll-cal.com/validate/${validationCode}`
+	+ `||${encodeURIComponent(email.replace(/\./g, "%2E"))}`
+	+ `||${encodeURIComponent(username.replace(/\./g, "%2E"))}`;
 
+	return emailTransport.sendMail({
+		from: "Roll-Cal.com <noreply@roll-cal.com>",
+		html: `
+		<p><img src="https://www.roll-cal.com/images/header-logo.png" alt="Roll-Cal.com" width="305" height="100" /></p>
+		<p>Hey there, ${username}!</p>
+		<p>You recently changed your email address on Roll-Cal.com.  If you meant to do this, click the link below to validate your email:</p>
+		<p>http://www.roll-cal.com/validate/${validationUrl}</p>
+		<p>If you didn't mean to do this, please go to https://www.roll-cal.com/contact and drop us a line so we can look into the issue.</p>
+		`,
+		subject: "Validate your Roll-Cal account",
+		to: `${username} <${email}>`,
+	}, null);
 
+};
