6ad6b63e910661e27c7f670b0dc5f105b214a568
Kevin Joseph <kjoseph@rainbird.com>
2018-11-02 17:03:24 -0700

    Save venue changes

--- src/routes/venues.ts
+++ src/routes/venues.ts
@@ -1,7 +1,12 @@
 import { Request, Response, Router } from "express";
+import multer from "multer";
 import { MysqlError } from "mysql";
 
+import { checkSession } from "checkSession";
+import { IRequestWithSession } from "interfaces";
+
 const router = Router();
+const upload = multer();
 
 router.get("/getAllVenues", (req: Request, res: Response) => {
 
@@ -59,6 +64,30 @@ router.get("/getVenueDetails/:id", (req: Request, res: Response) => {
 			}
 
 		});
+});
+
+router.put("/saveChanges", upload.array(), checkSession("user"), (req: IRequestWithSession, res: Response) => {
+
+	res.locals.connection
+		.query(`call saveVenueChanges(
+				${res.locals.connection.escape(req.session.user.id)},
+				${res.locals.connection.escape(req.body.id)},
+				${res.locals.connection.escape(req.body.changeObject)}
+			)`,
+
+			(error: MysqlError, results: any) => {
+				if (error) {
+					res.locals.connection.end();
+					console.error(error);
+					res.status(500).send();
+
+				} else {
+
+					res.status(200).send();
+
+				}
+			});
+
 });
 
 export default router;
