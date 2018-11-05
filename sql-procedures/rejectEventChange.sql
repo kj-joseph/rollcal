775988d87e797511f3bb50762be6a6cbf81567d8
Kevin Joseph <kj.joseph@gmail.com>
2018-11-04 22:16:22 -0800

    Create procedures for rejecting changes

---
+++ sql-procedures/rejectEventChange.sql
@@ -0,0 +1,31 @@
+BEGIN
+
+select c.change_user, true
+	into @user, @changeok
+from changes c
+where c.change_id = changeId
+	and c.changed_item_type = "event"
+	and c.change_status = "submitted";
+
+if @changeok = true then
+
+	update changes
+	set change_status = "rejected",
+		change_reviewer = reviewer,
+		change_comment = comment
+	where change_id = changeId;
+
+	select user_name, user_email
+		into @username, @email
+	from users
+	where user_id = @user;
+
+	select @username as username, @email as email, @user as user_id;
+
+else
+
+	select "Change not found." as error;
+
+end if;
+
+END
