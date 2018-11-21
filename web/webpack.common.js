b9f79fa21c99c8a2b514505dccda3d46aed1f195
Kevin Joseph <kj.joseph@gmail.com>
2018-11-20 17:22:59 -0800

    Remove unnecessary (and vulnerable) image-webpack-loader dependency

--- web/webpack.common.js
+++ web/webpack.common.js
@@ -98,25 +98,7 @@ module.exports = {
 							name: "[name].[ext]",
 							outputPath: "images/",
 						}
-					},
-					{
-						loader: "image-webpack-loader",
-						options: {
-							mozjpeg: {
-								progressive: true,
-								quality: 75,
-							},
-							optipng: {
-								enabled: false,
-							},
-							pngquant: {
-								enabled: false,
-							},
-							gifsicle: {
-								interlaced: false,
-							},
-						}
-					},
+					}
 				],
 			}
 		],
