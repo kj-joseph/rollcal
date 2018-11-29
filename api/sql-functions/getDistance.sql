af5061fe8eb82e6744bfa6d664cf55d188e0da08
Kevin Joseph <kj.joseph@gmail.com>
2018-11-28 16:40:27 -0800

    Get distance from submitted address when searching events

--- api/sql-functions/getDistance.sql
+++ api/sql-functions/getDistance.sql
@@ -2,18 +2,13 @@ BEGIN
 
 set @pi = pi();
 
-if units = "km" then
-	set @mult = 1.60934;
-else set @mult = 1;
-end if;
-
 return (((acos(
 			sin((lat1 * @pi / 180))
 			* sin((lat2 * @pi / 180))
 			+ cos((lat1 * @pi /180))
 			* cos((lat2 * @pi / 180))
-			* cos(((lon1 - lon2) * @pi / 180)) ))
+			* cos(((lng1 - lng2) * @pi / 180)) ))
 		* 180 / @pi )
-	* 60 * 1.1515 * @mult);
+	* 60 * 1.1515);
 
 END
--- api/src/routes/events.ts
+++ api/src/routes/events.ts
@@ -2,8 +2,9 @@ import { Request, Response, Router } from "express";
 import multer from "multer";
 import { MysqlError } from "mysql";
 
-import { IRequestWithSession } from "interfaces";
+import { IGeocode, IRequestWithSession } from "interfaces";
 import { checkSession } from "lib/checkSession";
+import { getGeocode } from "lib/googleMaps";
 
 import { sendChangeApprovalEmail, sendChangeRejectionEmail } from "lib/email";
 
@@ -42,39 +43,73 @@ router.get("/getEventDetails/:eventId", (req: Request, res: Response) => {
 
 router.get("/search", (req: Request, res: Response) => {
 
-	// procedure parameters: user, start date, end date, derbytypes, sanctions, tracks, locations
-	res.locals.connection.query(
-		`call searchEvents(
-			${req.query.user ? res.locals.connection.escape(req.query.user) : null},
-			${req.query.startDate ? res.locals.connection.escape(req.query.startDate) : null},
-			${req.query.endDate ? res.locals.connection.escape(req.query.endDate) : null},
-			${req.query.derbytypes ? res.locals.connection.escape(req.query.derbytypes) : null},
-			${req.query.sanctions ? res.locals.connection.escape(req.query.sanctions) : null},
-			${req.query.tracks ? res.locals.connection.escape(req.query.tracks) : null},
-			${req.query.locations ? res.locals.connection.escape(req.query.locations) : null},
-			${req.query.start ? res.locals.connection.escape(req.query.start) : null},
-			${req.query.count ?
-				(req.query.count === "all"
-					? 65535 : res.locals.connection.escape(req.query.count))
-				: null}
-		)`, (eventError: MysqlError, eventResults: any) => {
+	let lat: number = null;
+	let lng: number = null;
+	let geocodeError = false;
 
-			if (eventError) {
-				res.locals.connection.end();
-				console.error(eventError);
-				res.status(500).send();
+	const runSearchQuery = () => {
 
-			} else {
+		// procedure parameters: user, start date, end date, derbytypes, sanctions, tracks, locations, latitude, longitute, distance limit, start, count
+		res.locals.connection.query(
+			`call searchEvents(
+				${req.query.user ? res.locals.connection.escape(req.query.user) : null},
+				${req.query.startDate ? res.locals.connection.escape(req.query.startDate) : null},
+				${req.query.endDate ? res.locals.connection.escape(req.query.endDate) : null},
+				${req.query.derbytypes ? res.locals.connection.escape(req.query.derbytypes) : null},
+				${req.query.sanctions ? res.locals.connection.escape(req.query.sanctions) : null},
+				${req.query.tracks ? res.locals.connection.escape(req.query.tracks) : null},
+				${!req.query.address && req.query.locations ? res.locals.connection.escape(req.query.locations) : null},
+				${lat && !geocodeError ? res.locals.connection.escape(lat) : null},
+				${lng && !geocodeError ? res.locals.connection.escape(lng) : null},
+				${lat && lng && !geocodeError && req.query.distance ? res.locals.connection.escape(req.query.distance) : null},
+				${req.query.start ? res.locals.connection.escape(req.query.start) : null},
+				${req.query.count ?
+					(req.query.count === "all"
+						? 65535 : res.locals.connection.escape(req.query.count))
+					: null}
+			)`, (eventError: MysqlError, eventResults: any) => {
 
-				res.locals.connection.end();
-				res.status(200).json({
-					events: eventResults[0].map((row: {}) => ({...row})),
-					total: eventResults[1].map((row: {}) => ({...row}))[0].eventCount,
-				});
+				if (eventError) {
+					res.locals.connection.end();
+					console.error(eventError);
+					res.status(500).send();
 
-			}
+				} else {
 
-	});
+					res.locals.connection.end();
+					res.status(200).json({
+						events: eventResults[0].map((row: {}) => ({...row})),
+						total: eventResults[1].map((row: {}) => ({...row}))[0].eventCount,
+					});
+
+				}
+
+		});
+	};
+
+	if (req.query.address && req.query.distance) {
+
+		getGeocode(JSON.parse(req.query.address))
+			.then((geocode: IGeocode) => {
+
+				if (geocode) {
+					({lat, lng} = geocode);
+				} else {
+					geocodeError = true;
+				}
+
+				runSearchQuery();
+
+			}).catch((error: Error) => {
+				console.error(error);
+				geocodeError = true;
+			});
+
+	} else {
+
+		runSearchQuery();
+
+	}
 
 });
 
