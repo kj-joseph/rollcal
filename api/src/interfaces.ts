f0286972fcdad383f8ebe475b521c353a19db0ee
Kevin Joseph <kj.joseph@gmail.com>
2018-11-28 19:00:01 -0800

    Remove address2 field from address object for geocoding

--- api/src/interfaces.ts
+++ api/src/interfaces.ts
@@ -2,7 +2,6 @@ import { Request } from "express";
 
 export interface IAddressObject {
 	address1: string;
-	address2?: string;
 	city: string;
 	country: string;
 	postcode?: string;
@@ -13,7 +12,6 @@ export interface IDBVenueAddress {
 	country_name: string;
 	region_abbreviation?: string;
 	venue_address1: string;
-	venue_address2?: string;
 	venue_city: string;
 	venue_postcode?: string;
 }
