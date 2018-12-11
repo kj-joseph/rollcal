31faae89224a77050a1d0fb29fe87800c8ff311e
Kevin Joseph <kj.joseph@gmail.com>
2018-12-10 17:41:28 -0800

    Add timezone data to venue procedures

--- api/sql-procedures/getAllVenues.sql
+++ api/sql-procedures/getAllVenues.sql
@@ -1,13 +1,15 @@
 BEGIN
 
-select v.*, c.*, r.*, u.user_id, u.user_name
+select v.*, c.*, r.*, tz.*, u.user_id, u.user_name
 from venues v
-	join users u
+	left join users u
 		on v.venue_user = u.user_id
 	left join regions r
     	on v.venue_region = r.region_id
     left join countries c
     	on v.venue_country = c.country_code
+    left join timezones tz
+    	on v.venue_timezone = tz.timezone_id
 order by venue_name;
 
 END
